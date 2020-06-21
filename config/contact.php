e336428aec3b39927f09908533aede33574a313e
Kukorimam Markus <kuko@Kukorimams-MacBook-Pro.local>
2020-06-21 20:45:01 +0100

    Changes

--- config/contact.php
+++ config/contact.php
@@ -7,18 +7,14 @@
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="robots" content="all,follow">
-    <meta http-equiv="refresh" content="5;url=http://www.kayzmark.com/" />
+    <meta http-equiv="refresh" content="5;url=http://www.kayzmark.github.io/" />
 
-    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
-    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">
-    <link rel="stylesheet" href="../css/font-awesome-animation.min.css">
-    <link rel="stylesheet" href="../css/bootstrap.css">
-    <link rel="stylesheet" href="../css/bootstrap.css.map">
-    <link rel="stylesheet" href="../css/onepage-scroll.css">
-    <link rel="stylesheet" href="../css/style.default.css" id="theme-stylesheet">
-    <link rel="stylesheet" href="../css/custom.css">
-    <link rel="stylesheet" href="../css/bootstrap-lightbox.css">
-    <link rel="shortcut icon" href="../img/logo.png">
+    <link rel="stylesheet" href="css/uikit.min.css" />
+    <link rel="stylesheet" href="css/custom.css" />
+    <link rel="shortcut icon" href="img/logo.jpg">
+    <script src="js/uikit.min.js"></script>
+    <script src="js/uikit-icons.min.js"></script>
+    <link rel="shortcut icon" href="/img/logo.jpg">
   </head>
   <body>
     <style media="screen" type="text/css">
@@ -36,8 +32,8 @@
     </style>
     <?php
       $from = 'A Visitor<contact@kayzmark.com>';
-      $sendTo = 'Kukorimam Markus<kayzmark@gmail.com>';
-      $subject = 'I Visted Your Website!';
+      $sendTo = 'Kukorimam Markus<kukorimam@live.com>';
+      $subject = 'Website Contact Form';
       $fields = array('name' => 'Name', 'email' => 'Email', 'message' => 'Message'); // array variable name => Text to appear in email
       $okMessage = '<h3>Contact form successfully submitted. Thank you, I will get back to you soon!</h3><br/><p>Page will redirect in 5 secs...</p>';
       $errorMessage = '<h3>There was an error while submitting the form. Please try again later</h3><br/><p>Page will redirect in 5 secs...</p>';
